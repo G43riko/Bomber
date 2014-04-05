3ec8d06f8ab42ccc030fc9b6d7e11ed173495751
G43riko <gcsollei@hotmail.com>
2014-04-05 08:00:07 +0200

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -1,3 +1,4 @@
+//deklaruje pole mapy
 var mapa= [
   [1,1,1,1,1,1,1,1,1,1],
   [1,0,0,0,0,0,0,0,0,1],
@@ -10,6 +11,7 @@ var mapa= [
   [1,0,0,0,0,0,0,0,0,1],
   [1,1,1,1,1,1,1,1,1,1]
 ];
+//zikly ktoré spravia pre kažný prvok mape určitú operáciu
 for(var i=0;i<mapa.length;i++){
   for(var j=0;j<mapa[i].length;j++){
     if(mapa[i][j]==1){
