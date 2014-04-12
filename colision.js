2e167cb6cffa096874e1ab20c478fe2303276452
G43riko <gcsollei@hotmail.com>
2014-04-12 09:39:24 +0200

    Update colision.js

--- colision.js
+++ colision.js
@@ -1,4 +1,4 @@
-//this is basic colision
+//this is basic RectRect colision
 function colision(a,b){
   if((b.x+b.width>a.x)&&
     (b.y+b.height>a.y)&&
@@ -9,4 +9,21 @@ function colision(a,b){
 return 0;
 }
 //a,b are objects with coordinates, width and height
-//function return 1 if colision is true. else return 0
+
+
+//this is basic CircleCircle colision
+function acolision(a,b){
+  dst_x=a.x-b.x;
+  dst_y=a.y-b.y;
+  rad_a=a.size;
+  rad_b=b.size;
+  dist = sqrt((distX * distX) + (distY * distY));
+  if(dist <=(rad_a+rad_b)){
+    return 1;
+  }  
+  return 0;
+}
+//a,b are objects with coordinates and size
+
+
+//both functions return 1 if colision is true. else return 0
